26e4ae5206832f5814c80b5905a0c3a359fa3961
Fanera <egor.powwer@gmail.com>
2025-04-26 11:13:46 +1000

    rotate test

--- Scripts/Game/visual.py
+++ Scripts/Game/visual.py
@@ -1,12 +1,22 @@
 import pygame as pg
-
-
+from math import atan2,pi
+from pygame.math import Vector2
 class visual(pg.sprite.Sprite):
-    def set_sprite(self, image):
-        self.image = pg.image.load(image)
 
     def sprite_move(self,screen,pos):
         screen.blit(self.image,pos)
 
-    def set_sprite_size(self, new_size):
-        self.image = pg.transform.scale(self.image, new_size)
+    def set_sprite(self, image,pos):
+        self.image = pg.image.load(image)
+        self.orig_image = self.image
+        self.rect = self.image.get_rect(center = pos)
+        self.pos = Vector2(pos)
+    def update(self):
+        self.rotate()
+    def rotate(self):
+        direction = pg.mouse.get_pos()-self.pos
+        angle = direction.as_polar()[1]
+        self.image = pg.transform.rotate(self.orig_image,-angle)
+        self.rect = self.image.get_rect(center = self.rect.center)
+    def set_sprite_size(self,size):
+        self.orig_image = pg.transform.scale(self.orig_image,size)
--- test.py
+++ test.py
@@ -3,21 +3,22 @@ import pygame as pg
 
 pg.init()
 
-height = 500
-width = 500
+height = 1000
+width = 1000
 screen_size = (width,height)
 screen = pg.display.set_mode(screen_size)
 
 player = p.Player()
 player.set_stats(1,1,0.1,1)
-player.set_sprite("Scripts\Game\General\Assets\PlayerImage.jpg")
+x,y = height//2,width//2
+player.set_sprite("Scripts\Game\General\Assets\PlayerImage.jpg",(x,y))
 size = (100,100)
 player.set_sprite_size(size)
-x,y = height//2,width//2
 
 run = True
 while run:
     screen.fill((0,0,0))
+    player.rotate()
     player.sprite_move(screen,(x,y))
     for event in pg.event.get():
         if event.type == pg.QUIT:
