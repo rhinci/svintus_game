820a594034fc309a76aaa60374351a77931e9205
Fanera <egor.powwer@gmail.com>
2025-05-18 13:04:01 +1000

    меню и сцены

---
+++ Menu.py
@@ -0,0 +1,68 @@
+import pygame
+import sys
+from Scripts.Menu.buttons_class import Button
+from configs.screen_config import SIZE, HEIGHT, WIDTH
+from configs.btns_config import BUTTON_DEFINITIONS
+from easy_mode import game_scene
+def main_menu():
+    pygame.init()
+
+    screen = pygame.display.set_mode(SIZE)
+    pygame.display.set_caption("Rusostrus")
+
+    background = pygame.image.load("Assets\_UIMenu\Background.png").convert()
+    background = pygame.transform.scale(background, (WIDTH, HEIGHT))
+
+    buttons = pygame.sprite.Group()
+    button_instances = {}
+
+    for btn_def in BUTTON_DEFINITIONS:
+        btn = Button(screen, 0.35, btn_def["y_pos"], 0.3, 0.1, "", btn_def["image"], btn_def["hover_image"])
+        buttons.add(btn)
+        button_instances[btn_def["name"]] = btn
+
+    running = True
+    while running:
+        screen.blit(background, (0, 0))
+
+        font_size = int(HEIGHT * 0.1)
+        pixel_font = pygame.font.Font("Assets/Lover-unity.otf", font_size)
+        text_surface = pixel_font.render("Rusostrus", True, (0, 0, 0))
+        text_rect = text_surface.get_rect(center=(WIDTH // 2, HEIGHT // 6))
+        screen.blit(text_surface, text_rect)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.USEREVENT:
+                if event.button == button_instances["easy"]:
+                    pygame.quit()
+                    game_scene()
+                    # точка входа в изи мод
+
+                elif event.button == button_instances["hard"]:
+                    print("Hard mode selected")
+                    # точка входа в хард мод
+
+                elif event.button == button_instances["weapon"]:
+                    print("Open weapon selection")
+                    # вход в меню выбора оружия
+
+                elif event.button == button_instances["exit"]:
+                    print("Exiting game")
+                    pygame.quit()
+                    sys.exit()
+
+            for btn in button_instances.values():
+                btn.handle_event(event)
+
+        mouse_pos = pygame.mouse.get_pos()
+        for btn in button_instances.values():
+            btn.check_hover(mouse_pos)
+
+        buttons.draw(screen)
+
+        pygame.display.flip()
---
+++ easy_mode.py
@@ -0,0 +1,50 @@
+import pygame as pg
+from Scripts.Game.General.Player import Player
+from Scripts.Game.Weapon_scripts.Mashinegun import mashineGun
+from Scripts.Game.Weapon_scripts.RocketLauncher import rocketlauncher
+from Scripts.Game.Weapon_scripts.Mashinegun import mashineGun
+from Scripts.Game.Weapon_scripts.LaserGun import lasergun
+from Scripts.Game.General import Spawner
+from configs.character_config import PLAYER
+from configs.weapon_config import MASHINEGUN,LASERGUN,ROCKETLAUNCHER
+from configs.screen_config import SIZE,HEIGHT,WIDTH
+def game_scene():
+    #инициализация основных систем
+    pg.init()
+    clock = pg.time.Clock()
+    FPS = 60
+    screen = pg.display.set_mode(SIZE)
+    running = True
+
+    #группы
+    all_sprites = pg.sprite.Group()
+    weapon_group = pg.sprite.Group()
+    player_group = pg.sprite.Group()
+    enemy_group = pg.sprite.Group()
+    mobs_group = pg.sprite.Group()
+
+    #игрок
+    pos = (WIDTH/2,HEIGHT/2)
+    player = Player(all_sprites,player_group,mobs_group,PLAYER,pos)
+    mashineGun_ = lasergun(all_sprites,mobs_group,enemy_group,weapon_group,LASERGUN,player.pos)
+    player.set_weapon(mashineGun_)
+
+    #враги
+    spawner = Spawner.spawner(all_sprites,enemy_group,mobs_group,player,1,SIZE)
+
+    while running:
+        clock.tick(FPS)
+        screen.fill((0,0,0))
+        all_sprites.draw(screen)
+        all_sprites.update()
+        spawner.update()
+        for event in pg.event.get():
+            if event.type == pg.QUIT or event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
+                running = False
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_1:
+                    player.change_weapon(mashineGun(all_sprites,mobs_group,enemy_group,weapon_group,MASHINEGUN,player.pos))
+                if event.key == pg.K_2:
+                    player.change_weapon(rocketlauncher(all_sprites,mobs_group,enemy_group,weapon_group,ROCKETLAUNCHER,player.pos))
+        pg.display.flip()
+    pg.quit()
