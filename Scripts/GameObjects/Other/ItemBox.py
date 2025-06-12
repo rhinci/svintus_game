ee3bd6ec3e73ccc1f784cd317701911409b1ea2e
Fanera <egor.powwer@gmail.com>
2025-06-13 02:40:36 +1000

    звуки2

--- Scripts/GameObjects/Other/ItemBox.py
+++ Scripts/GameObjects/Other/ItemBox.py
@@ -7,8 +7,11 @@ class itemBox(pg.sprite.Sprite):
         #инициализация отображения
         super().__init__(all_sprites)
         self.item_type = config['item_type']
-        self.image = pg.image.load(config['image'])
+        self.image = pg.transform.scale(pg.image.load(config['image']),config['size'])
         self.rect = self.image.get_rect()
+        self.sound = pg.mixer.Sound(config['sound'])
+        self.sound.set_volume(0.5)
+        self.channel = pg.mixer.Channel(6)
         self.rect.midtop = pos
         self.player = player
 
@@ -23,6 +26,7 @@ class health_pack(itemBox):
     def collision(self):
         #взаимодействие с игроком
         if pg.sprite.collide_rect(self, self.player):
+            self.sound.play()
             self.player.change_hp(5)
             self.kill()
 
@@ -31,6 +35,8 @@ class exp_pack(itemBox):
     def collision(self):
         #взаимодействие с игроком
         if pg.sprite.collide_rect(self, self.player):
+            if not(self.channel.get_busy()):
+                self.sound.play()
             self.player.change_exp(1)
             run_stats.increment_stat("3. Crystals collected")
             self.kill()
--- configs/collectables.py
+++ configs/collectables.py
@@ -1,8 +1,12 @@
 HEALTH_PACK = {
     "item_type": "health_pack",
-    "image": "Assets\other\Heart.png"
+    "image": "Assets\other\Heart.png",
+    "size": (20,20),
+    "sound": "Assets\Sound\other\heal.mp3"
 }
 EXP = {
     "item_type": "EXP",
-    "image": "Assets\other\EXP.png"
+    "image": "Assets\other\EXP.png",
+    "size": (20,20),
+    "sound": "Assets\Sound\other\exp.mp3"
 }
